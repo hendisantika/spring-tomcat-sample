7386e4d1c92f57663ec26807a8c722bddbb9f88f
Hendi Santika <hendisantika@gmail.com>
2017-12-07 06:18:30 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springtomcatsample/SpringTomcatSampleApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springtomcatsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTomcatSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTomcatSampleApplication.class, args);
+	}
+}
