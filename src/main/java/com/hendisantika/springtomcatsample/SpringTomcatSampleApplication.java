c38a3299188c233c235f59cb59d68690d4d85635
Hendi Santika <hendisantika@gmail.com>
2017-12-07 06:24:57 +0700

    #5 - Modify Main App

--- src/main/java/com/hendisantika/springtomcatsample/SpringTomcatSampleApplication.java
+++ src/main/java/com/hendisantika/springtomcatsample/SpringTomcatSampleApplication.java
@@ -1,12 +1,17 @@
 package com.hendisantika.springtomcatsample;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringTomcatSampleApplication {
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringTomcatSampleApplication.class, args);
-	}
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "com.hendisantika.springtomcatsample")
+public class SpringTomcatSampleApplication extends AnnotationConfigWebApplicationContext {
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(SpringTomcatSampleApplication.class, args);
+//	}
 }
