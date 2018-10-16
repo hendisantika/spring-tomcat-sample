30abdc96707ac8495369cfc9c6548c0264f0f8ed
Hendi Santika <hendisantika@gmail.com>
2018-10-16 07:07:25 +0700

    #18 - Modify data.sql file

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,10 @@
 INSERT INTO person(first_name, last_name, date_of_birth, place_of_birth)
-VALUES ('First', 'Last', DATE '1990-02-21', 'Bangalore');
+VALUES ('First', 'Last', DATE '1990-02-21', 'Tokyo');
 INSERT INTO person(first_name, last_name, date_of_birth, place_of_birth)
-VALUES ('First2', 'Last2', DATE '1987-02-21', 'Mumbai');
+VALUES ('First2', 'Last2', DATE '1987-02-21', 'Konoha');
 INSERT INTO person(first_name, last_name, date_of_birth, place_of_birth)
-VALUES ('First3', 'Last3', DATE '1996-02-21', 'Chennai');
+VALUES ('First3', 'Last3', DATE '1996-02-21', 'Cimahi');
 INSERT INTO person(first_name, last_name, date_of_birth, place_of_birth)
-VALUES ('First4', 'Last4', DATE '1978-02-21', 'Delhi');
+VALUES ('First4', 'Last4', DATE '1978-02-21', 'Bandung');
+
+VALUES ('First4', 'Last4', DATE '1978-02-21', 'Jakarta');
